b21ceee31cc3ee91f506db177e1f1e28b4859518
Fred Wu <canglan@gmail.com>
2009-12-02 18:11:19 +1100

    - Major rewrite: slides can now be clicked freely (as opposed to waiting for one to finish showing before it can be clicked again) - Added automatic photo caption

--- js/jquery.slideshow.lite-0.5.js
+++ js/jquery.slideshow.lite-0.5.js
@@ -1,7 +1,7 @@
 /**
  * Slideshow Lite plugin for jQuery
  *
- * v0.4
+ * v0.5
  *
  * Copyright (c) 2009 Fred Wu
  *
@@ -16,6 +16,7 @@
  * pauseSeconds  integer  number of seconds between each photo to be displayed
  * width         boolean  width of the slideshow, in pixels
  * height        integer  height of the slideshow, in pixels
+ * caption       boolean  display caption?
  * cssClass      string   name of the CSS class, defaults to 'slideshowlite'
  */
 
@@ -26,6 +27,7 @@
 			pauseSeconds: 2,
 			width: 468,
 			height: 120,
+			caption: true,
 			cssClass: 'slideshowlite'
 		};
 		
@@ -35,9 +37,9 @@
 		// slideshow objects and variables
 		// ----------------------------------------
 		
-		var target  = this;
-		var items   = $(target).children("a");
-		var running = false; // ensures there is only one running instance
+		var target = this;
+		var items  = $(target).children("a");
+		var instance;
 		
 		// ----------------------------------------
 		// some mandontory styling
@@ -50,101 +52,123 @@
 			height: options.height + "px"
 		});
 		
+		// ----------------------------------------
 		// create anchor links to make the structure simpler for manupilation
+		// ----------------------------------------
+		
 		$(this).children("img").wrap(document.createElement("a"));
-		// force opening links in a new page
 		$(this).children("a").attr("target", "blank");
+		
+		// ----------------------------------------
 		// add item sequence markups
+		// ----------------------------------------
+		
 		var i = 1;
 		$(this).children("a").each(function(){
 			$(this).attr("rel", i++);
 		});
-		// create pagination
+		
+		// ----------------------------------------
+		// create pagination and caption
+		// ----------------------------------------
+		
 		$(this).append("<ul></ul>");
-		var pagination = $(target).children("ul");
+		$(this).append("<ol></ol>");
+		var pagination = $(this).children("ul");
+		var caption = $(this).children("ol");
+		
 		var i = 1;
+		var j = 0;
 		$(this).children("a").each(function(){
 			pagination.append("<li><a href=\"#\">" + i++ + "</a></li>");
+			caption.append("<li>" + $("#" + $(target).attr("id") + " img:nth(" + j++ + ")").attr("alt") + "</li>");
 		});
-		// make the pagination transparent
 		pagination.fadeTo(0, 0.8);
+		caption.fadeTo(0, 0.6);
+		caption.hide();
 		
 		// ----------------------------------------
 		// shortcuts
 		// ----------------------------------------
 		
-		var firstItem = $(target).children("a:first");
-		var lastItem  = $(target).children("a:last");
+		var firstItem   = $(target).children("a:first");
+		var lastItem    = $(target).children("a:last");
+		var currentItem = firstItem;
 		
 		// ----------------------------------------
 		// pagination highlight
 		// ----------------------------------------
 		
 		var paginationHighlight = function(sequence){
-			pagination.children("li").children("a").fadeTo(100, 1);
 			pagination.children("li").children("a").removeClass("current");
 			pagination.children("li").children("a:nth(" + sequence + ")").addClass("current");
+		}
+		
+		// ----------------------------------------
+		// caption
+		// ----------------------------------------
+		
+		var showCaption = function(sequence){
+			caption.show();
+			caption.children("li").hide();
+			caption.children("li:nth(" + sequence + ")").fadeIn();
 		}
 		
 		// ----------------------------------------
 		// slideshow logic
 		// ----------------------------------------
 		
-		var makeSlideshow = function(current){
-			if (running == false)
-			{
-				current.fadeIn(function(){
-					$(target).children("a").hide();
-					$(this).show();
-				}).animate({opacity:1}, options.pauseSeconds*1000, function(){
-					
-					// rewind to the first item?
-					if (current.children("img").attr("src") == lastItem.children("img").attr("src"))
-						makeSlideshow(firstItem);
-					else
-						makeSlideshow(current.next());
-					
-				}).fadeOut();
-				
-				paginationHighlight(current.attr("rel")-1);
-			}
+		var makeSlideshow = function(){
 			
-			// pagination click
 			pagination.children("li").children("a").click(function(){
-				
-				if (running == false && ! $(this).hasClass("current"))
+				if ( ! $(this).hasClass("current"))
 				{
-					running = true;
-					
-					var self = this;
-					$(target).children("a").hide();
-					
-					$(target).children("a:nth(" + ($(self).text()-1) + ")").show().animate({opacity:1}, options.pauseSeconds*1000, function(){
-						
-						running = false;
-						clicked = false;
-						
-						// rewind to the first item?
-						if ($(self).text() == $(target).children("a").length)
-							makeSlideshow(firstItem);
-						else
-							makeSlideshow($(target).children("a:nth(" + $(self).text() + ")"));
-						
-					}).fadeOut();
-					
-					paginationHighlight($(self).text()-1);
-					pagination.children("li").children("a").fadeTo(0, 0.2);
+					// select the current item after the pagination click
+					currentItem = $(target).children("a:nth(" + ($(this).text()-1) + ")");
+
+					currentItem.show();
+					startSlideshow(false);
 				}
-				
-				return false;
 			});
 			
+			// pagination highlight
+			paginationHighlight(currentItem.attr("rel")-1);
+			
+			// show caption
+			if (options.caption == true)
+			{
+				showCaption(currentItem.attr("rel")-1);
+			}
+			
+			// show the current slide
+			currentItem.fadeIn(function(){
+				$(target).children("a").hide();
+				$(this).show().css("z-index", 1);
+			});
+			
+			// prepare for the next slide
+			// determines the next item (or we need to rewind to the first item?)
+			if (currentItem.children("img").attr("src") == lastItem.children("img").attr("src"))
+			{
+				currentItem = firstItem;
+				currentItem.css("z-index", 2);
+			}
+			else
+			{
+				currentItem = currentItem.next();
+			}
+		};
+		
+		var startSlideshow = function(){
+			clearInterval(instance);
+			makeSlideshow();
+			instance = setInterval(makeSlideshow, options.pauseSeconds*1000);
 		};
 		
 		// ----------------------------------------
 		// start the slideshow!
 		// ----------------------------------------
 		
-		makeSlideshow(firstItem);
+		startSlideshow(true);
 	};
 })(jQuery);
